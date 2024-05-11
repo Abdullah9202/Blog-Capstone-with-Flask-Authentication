b5416ce84b63f1618a530fbe0a99ada7f43f01a0
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-05-11 18:24:44 +0500

    Initital Fixes

--- main.py
+++ main.py
@@ -3,75 +3,71 @@ from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from flask_gravatar import Gravatar
 # My Files (Classes)
 from classes.forms import CreatePostForm
+from classes.blogPost import BlogPost, db
 
-
+# Init Flask App
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
+# CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# Init the Flask App with SQL DB
+db.init_app(app)
 
 
-##CONFIGURE TABLES
-
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(250), nullable=False)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-
-
+# Home Route
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
     return render_template("index.html", all_posts=posts)
 
 
+# Register Route
 @app.route('/register')
 def register():
     return render_template("register.html")
 
 
+# Login Route
 @app.route('/login')
 def login():
     return render_template("login.html")
 
 
+# Logout Route
 @app.route('/logout')
 def logout():
     return redirect(url_for('get_all_posts'))
 
 
+# Show Post Route
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
     return render_template("post.html", post=requested_post)
 
 
+# About Route
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
+# Contact Route
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
 
+# New Post Route
 @app.route("/new-post")
 def add_new_post():
     form = CreatePostForm()
@@ -90,6 +86,7 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
+# Edit Post Route
 @app.route("/edit-post/<int:post_id>")
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
@@ -112,6 +109,7 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form)
 
 
+# Delete Post Route
 @app.route("/delete/<int:post_id>")
 def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
@@ -120,7 +118,9 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
+# Executing as Script
 if __name__ == "__main__":
+    # Creating a database with app context
     with app.app_context():
         db.create_all()
     app.run(host='127.0.0.1', port=5000)
