397afca3e571dae86d578c4078d1c141309f752f
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-05-12 22:50:39 +0500

    User Class Updated

--- classes/user_class.py
+++ classes/user_class.py
@@ -16,7 +16,7 @@ class RegisterForm(FlaskForm):
 
 # User Class (DB)
 class User(UserMixin, db.Model):
-    __tablename__ = "registered_users"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
