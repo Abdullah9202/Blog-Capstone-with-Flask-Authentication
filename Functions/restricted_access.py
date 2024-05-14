9ce647768ea9be6ce3389e1548bb76c267fef241
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-05-14 10:12:56 +0500

    Circular Import Error Removed

--- Functions/restricted_access.py
+++ Functions/restricted_access.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask import abort
 # My Files (main.py)
-from main import current_user
+from flask_login import current_user
 
 # Creating the admin access only function
 def admin_only(f):
