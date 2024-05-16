fd12c2b8f71723892c650e449de76d3d1fed805e
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-05-16 18:15:48 +0500

    Setup for relational DBs

--- classes/user_class.py
+++ classes/user_class.py
@@ -1,9 +1,14 @@
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email,URL
+from wtforms.validators import DataRequired, Email, URL
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 # My Files (Classes)
 from classes.blogPost import db
+
+# Declarative Base for Mapped Classes
+Base = declarative_base()
 
 # Custom Password Validation can be created for the login and registeration forms
 
@@ -21,9 +26,11 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Log in")
 
 # User Class (DB)
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model, Base):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    # Defining one to many relationships with posts
+    posts = relationship("Posts", back_populates="user")
