10ec1738ef5b95d3505c2d33ae81d7ccd39093f8
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-05-17 09:26:46 +0500

    Bidirectional relationship established

--- classes/blogPost.py
+++ classes/blogPost.py
@@ -1,14 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 # My Files (Classes)
-import 
+from classes.user_class import Base
 
 db = SQLAlchemy()
 
 # Blog Post Class
-class BlogPost(db.Model):
+class BlogPost(db.Model, Base):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(250), nullable=False)
+    # Getting the author's ID
+    author_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+    # Defining many to one relationship with user
+    author_rel = relationship("User", back_populates="posts")
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
